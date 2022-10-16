2185305369443dddd64fef5c328cd5fecc8ce773
jkumagai96 <jkumagai96@gmail.com>
2022-10-16 16:22:36 -0700

    Final data table w/ kelp data and mpa status

---
+++ Scripts/04_extract_mpa_data.R
@@ -0,0 +1,70 @@
+# Date: October 16th 2022
+# Author: Joy Kumagai (kumagaij@stanford.edu) 
+# Purpose: Create a data table with MPA protection per point with kelp data
+# BIO 202: Ecological Statistics
+
+##### Set up: packages #########################################################
+# load packages 
+library(sf)
+library(tidyverse)
+
+##### Set up: load data ########################################################
+mpas_original <- read_sf("Data/Filtered_MPAs/MPAs_CA_Giant_Kelp.shp")
+station_data <- read.csv("Processed_data/data_tables/PixelID_reference.csv")
+kelp_data <- read.csv("Processed_data/data_tables/kelp_data.csv")
+
+##### Formatting ###############################################################
+
+# Filter kelp points to latitude 37.4
+kelp_data <- kelp_data %>% 
+  filter(lat <= 37.4)
+
+# Select needed attribuets from MPAs
+mpas <- mpas_original %>% 
+  mutate(mpa_status = replace(Prot_Lvl_1, 
+                          Prot_Lvl_1 == "No Access", 
+                                        "Partial")) %>% # QUESTION CHECK THIS WITH NUR
+  select(Site_ID_12, Estab_Yr_1, AreaMar_12, mpa_status)  %>% # I selected Area_Mar12, and not Area? QUESTION
+  
+  # Match projections with kelp data 
+  st_transform(crs = 4326) 
+
+# Filter station points to latitude 37.4
+station_data <- station_data %>% 
+  filter(y <= 37.4)
+
+# convert station points into spatial data to intersect with mpas 
+station_points <- st_as_sf(station_data, 
+                           coords = c("x", "y"), 
+                           crs = 4326) 
+
+# Make sure the projections match 
+crs(station_points) == crs(mpas)
+
+
+##### Processing ###############################################################
+# Spatial intersect between mpas and points to get all points within mpas 
+points_in_mpas <- st_intersection(station_points, mpas) %>% 
+  select(PixelID, mpa_status, Site_ID_12) %>% 
+  st_drop_geometry
+
+# Join data with kelp data 
+kelp_w_mpas <- left_join(kelp_data, points_in_mpas, by = "PixelID") 
+
+# Format values of mpa_status so NA is none, no take is full, and uniform multiple use is partial
+kelp_w_mpas$mpa_status[is.na(kelp_w_mpas$mpa_status)] <- "None"
+kelp_w_mpas$mpa_status[kelp_w_mpas$mpa_status == "No Take"] <- "Full"
+kelp_w_mpas$mpa_status[kelp_w_mpas$mpa_status == "Uniform Multiple Use"] <- "Partial"
+
+# Formatting findal data 
+final_data <- kelp_w_mpas %>% 
+  rename("Mpa_ID" = "Site_ID_12") %>% 
+  relocate(Mpa_ID, .after = depth) %>% 
+  relocate(mpa_status, .after = Mpa_ID)
+
+##### Export ###################################################################
+write.csv(final_data, "Processed_data/data_tables/kelp_data_w_mpas.csv", row.names = F)
+
+
+
+
