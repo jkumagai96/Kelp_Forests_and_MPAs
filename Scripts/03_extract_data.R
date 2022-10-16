b234e6ef8a60e301ba3f441a7e50702da1bdd82d
jkumagai96 <jkumagai96@gmail.com>
2022-10-16 14:47:33 -0700

    Final kelp dataset from NetCDF

--- Scripts/03_extract_data.R
+++ Scripts/03_extract_data.R
@@ -1,6 +1,6 @@
 # Date: October 14th 2022
 # Author: Joy Kumagai (kumagaij@stanford.edu) 
-# Purpose: Extract raster data into table and create pixel ID dataset
+# Purpose: Extract raster data into table 
 # BIO 202: Ecological Statistics
 
 ##### Set up: packages #########################################################
@@ -21,7 +21,7 @@ depth_r <- raster(here(
 
 # Create points out of depth raster
 depth_p <- rasterToPoints(depth_r) %>% 
-  as.data.frame %>% 
+  as.data.frame() %>% 
   mutate(PixelID = 1:nrow(.))
 
 # export
@@ -30,26 +30,68 @@ write.csv(depth_p,
           row.names = F
 )
 
-##### Create Datables for each variable ########################################
+##### Create a datatable with all variables ####################################
+## Initialize global variables 
+years <- 2009:2021                # years of the data we are working with 
+PixelID <- 1:nrow(depth_p)        # ID just in case we need to join variables not in order
+kelp_list <- list()               # Final list all data will be in 
 
-area_files <- list.files("Processed_data/kelp_variables", pattern = "area_20", full.names = T)
-years <- 2009:2021
-PixelID <- 1:nrow(depth_p)
+# Variables we are working in that will go through each iteration of the 
+# outer loop (j) 
+variables <- c("area", "biomass", "hsmax", "nitrate", "temperature")
 
-# load data 
-r <- raster(area_files[1])
+# Outer loop goes through variables 
+for (j in 1:length(variables)) {
+  variable_files <- list.files("Processed_data/kelp_variables", pattern = variables[j], full.names = T)
+  variable_all <- c()
+  
+  # Inner loop goes through each year of data for the variable 
+  for (i in 1:length(years)) {
+    # load data 
+    r <- raster(variable_files[i])
+    
+    # convert to df
+    data_yr <- rasterToPoints(r) %>% 
+      as.data.frame() %>% 
+      mutate(year = years[i],
+             PixelID = PixelID) %>% 
+      select(x, y, PixelID, year, layer)
+    
+    # Adds all the year data together to the bottom of the data frame 
+    variable_all <- rbind(variable_all, data_yr) 
+    
+  } # end of inner loop 
+  
+  
+  # Assigns the name of the column (variable)
+  colnames(variable_all)[5] <- variables[j]     
+  
+  # Adds each variable into the final list 
+  kelp_list[[j]] <- variable_all                
 
-# convert to df
-area_data <- rasterToPoints(r) %>% 
-  as.data.frame() %>% 
-  rename("area" = "layer") %>% 
-  mutate(year = years[1],
-         PixelID = PixelID) %>% 
-  select(x, y, PixelID, year, area)
-
-# Need to make a for loop that ends up with all the years in this data frame
-
-# All tested rasters have the same dimensions, let's try to join them the proper way. Cbind also works 
-new_dat2 <- cbind(as.data.frame(test), as.data.frame(test3)$depth)
+  }
 
 
+# Now we put each part of the list together so we have a data frame with 
+# Latitude, longitude, Pixel ID, year, area, biomass, hsmax, nitrate, and temperature
+
+# sets up the table with area dataset from the list 
+area <- left_join(kelp_list[[1]], depth_p) %>% 
+  rename("depth" = "layer") 
+
+# adds each variable to the table 
+biomass <- kelp_list[[2]]$biomass
+hsmax <- kelp_list[[3]]$hsmax
+nitrate <- kelp_list[[4]]$nitrate
+temperature <- kelp_list[[5]]$temperature
+
+# Putting the dataset together and formatting 
+kelp_all_data <- cbind(area, biomass, hsmax, nitrate, temperature) %>% 
+  rename("lon" = "x", 
+         "lat" = "y") %>% 
+  relocate(depth, .after = PixelID)
+
+##### Export ###################################################################
+write.csv(kelp_all_data, "Processed_data/data_tables/kelp_data.csv", row.names = F)
+
+# End of script
