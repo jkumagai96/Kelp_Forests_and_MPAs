70a4d17cb582ad9d79da114e3e935ee1fa374d10
jkumagai96 <jkumagai96@gmail.com>
2023-04-26 13:59:46 -0700

    Paralilzed clean version

--- Scripts/10_permutation_test_percent_recovery.R
+++ Scripts/10_permutation_test_percent_recovery.R
@@ -1,18 +1,20 @@
-# Date: February 13th 2023
+# Date: April 20th 2023
 # Author: Joy Kumagai (kumagaij@stanford.edu) 
-# Purpose: Bootstrap - Randomization approach based on difference in percent recovery instead of kelp area 
+# Purpose: Bootstrap - Permutation approach based on difference in percent recovery instead of kelp area 
 # BIO 202: Ecological Statistics
 
 ##### Set Up: Packages and Data ################################################
 # Packages
 library(tidyverse)
+library(doParallel)
+library(foreach)
 
 # Data 
 kelp_data_all <- read.csv("Processed_data/data_tables/kelp_data_all_variables_and_mpa_status_per_year.csv") 
 points_in_mpas <- read.csv("Processed_data/data_tables/Spatial_intersect_mpas_and_station_points.csv")
 
-# Would only be looking at data from Central and Southern Coast of California
-# Would only be looking at data from 2016-2021 due to calculating percent recovery 
+# Will only be looking at data from Central and Southern Coast of California
+# Will only be looking at data from 2016-2021 due to calculating percent recovery 
 
 ##### Declare Functions ########################################################
 Calculate_percent_recovery <- function(x, min_v, max_v) {
@@ -23,16 +25,14 @@ Calculate_percent_recovery <- function(x, min_v, max_v) {
 ##### Format Data ##############################################################
 data_area <- kelp_data_all %>% 
   filter(region == "South_Coast" | region == "Central_Coast") %>% 
-  filter(year > 1999 & year < 2014) %>% 
+  filter(year < 2014) %>% 
   select(PixelID, year, area) %>% 
   pivot_wider(names_from = year, values_from = area) 
 
 # Calculate Max and Min value 
 data_area$min <- apply(data_area, 1, FUN = min, na.rm = T)
 data_area$max <- apply(data_area, 1, FUN = max, na.rm = T)
-data_area$median <- apply(data_area, 1, FUN = median, na.rm = T)
 
-# Choosing mean as there are fewer zeros' 
 df <- data_area %>% 
   select(PixelID, min, max)
 
@@ -58,19 +58,19 @@ for (i in 1:nrow(df_percent_recovery )) {
 kelp_data <- kelp_data_all %>% 
   filter(region == "South_Coast" | region == "Central_Coast") %>% 
   filter(year >= 2016) %>% 
-  select(PixelID, year, mpa_status) %>% 
-  left_join(df_percent_recovery, by = c("PixelID", "year"))
+  select(PixelID, year, mpa_status, area) %>% 
+  left_join(df_percent_recovery, by = c("PixelID", "year", "area"))
 
 ##### Calculate True Values ####################################################
 # Change global option to not print out message about group summaries 
 options(dplyr.summarise.inform = FALSE)
 
-# Calculate average kelp area per category per year 
+# Calculate median percent kelp recovery per category per year for the real data
 true_values <- kelp_data %>% 
   group_by(year, mpa_status) %>% 
-  summarise(avg_pr = mean(percent_recovery)) %>% 
+  summarise(median_pr = median(percent_recovery)) %>% 
   pivot_wider(names_from = mpa_status,
-              values_from = avg_pr) %>% 
+              values_from = median_pr) %>% 
   mutate(P_N = Partial - None,
          F_N = Full - None,
          F_P = Full - Partial) %>% 
@@ -140,12 +140,12 @@ bootstrap_list <- foreach (j = 1:10000) %dopar% {
   subset <- subset %>% arrange(PixelID)
   new_data <- rbind(not_protected, subset)
   
-  # Calculate table 
+  # Calculate table, changes here too!
   values <- new_data %>% 
     group_by(year, mpa_status) %>% 
-    summarise(avg_pr = mean(percent_recovery)) %>% 
+    summarise(median_pr = median(percent_recovery)) %>% 
     pivot_wider(names_from = mpa_status,
-                values_from = avg_pr) %>% 
+                values_from = median_pr) %>% 
     mutate(P_N = Partial - None,
            F_N = Full - None,
            F_P = Full - Partial) %>% 
@@ -189,34 +189,24 @@ for (i in 1:length(years)) {
   
 }
 
-test <- as.data.frame(final_results <= 0.0004385965)
-test$year <- final_results$year
-
 ##### Export ###################################################################
 final_results
 
 write.csv(final_results, 
-          "Processed_data/data_tables/bootstrap_percent_recovery.csv", 
+          "Processed_data/data_tables/bootstrap_percent_recovery_median.csv", 
           row.names = F)
 
 print('bootstrap results saved')
 ##### Figures ##################################################################
-# Identify years of marine heat waves based on plot
-mhw_years <- data.frame(year = c(2014, 2015),
-                        mhw = 1)
-
 # Format Data into long format
 results_long <- final_results %>% 
-  pivot_longer(names_to = "Comparison", values_to = "pvalues", F_N:F_P) %>% 
-  left_join(mhw_years, by = "year") %>% # Join the data together 
-  mutate(mhw = replace_na(mhw, 0))
+  pivot_longer(names_to = "Comparison", values_to = "pvalues", F_N:F_P) 
 
 results_long$pvalues[results_long$pvalues == 0] <- .000001
 
 print('now attempting to make figure')
 # Plot 
 plot1 <- results_long %>% 
-  #filter(year >= 2008) %>% 
   ggplot(aes(x = year, y = -log10(pvalues), group = Comparison)) +
   geom_point(aes(color = Comparison, shape = Comparison), size = 2) +
   geom_hline(yintercept = -log10(0.05/(18))) +
@@ -225,15 +215,52 @@ plot1 <- results_long %>%
   theme_bw() +
   theme(legend.position = "bottom") +
   scale_x_continuous(breaks = c(2016, 2017, 2018, 2019, 2020, 2021)) +
-  #scale_x_continuous(breaks = c(1984, 1988, 1992, 1996, 2000, 2004, 2008, 2012, 2016, 2020)) +
   labs(y = "-log10(P values)", x = "Year") 
 
 # Export
 print('attempting export')
 
-png("Figures/Bootstrap_percent_recovery.png", width = 5, height = 3, units = "in", res = 600)
+png("Figures/Bootstrap_percent_recovery_median.png", width = 5, height = 3, units = "in", res = 600)
 plot1
 dev.off() 
 
+### Figure of Percent Recover through time all, then summarized by mpa status
+kelp_data %>% 
+  mutate(year = as.factor(year)) %>% 
+  ggplot(aes(x = year, y = percent_recovery)) +
+  geom_point(alpha = 0.2)
+
+# box plot not seperated by mpa status 
+kelp_data %>% 
+  mutate(year = as.factor(year)) %>% 
+  ggplot(aes(x = year, y = percent_recovery)) +
+  geom_boxplot() +
+  #geom_point(alpha = 0.2) +
+  ylim(0, 100)
+
+# box plot seperated by mpa status
+kelp_data %>% 
+  mutate(year = as.factor(year)) %>% 
+  ggplot(aes(x = year, y = percent_recovery, fill = mpa_status)) +
+  geom_boxplot() +
+  #geom_point(alpha = 0.2) +
+  ylim(0, 100)
+
+# points (mean)
+kelp_data %>% 
+  group_by(year, mpa_status) %>% 
+  summarize(mean_percent_recovery = mean(percent_recovery)) %>% 
+  ggplot(aes(x = year, y = mean_percent_recovery, color = mpa_status)) +
+  geom_line() +
+  geom_point()
+
+
+# points (median)
+kelp_data %>% 
+  group_by(year, mpa_status) %>% 
+  summarize(med_percent_recovery = median(percent_recovery)) %>% 
+  ggplot(aes(x = year, y = med_percent_recovery, color = mpa_status)) +
+  geom_line() +
+  geom_point()
+
 print('script is finished')
-  
